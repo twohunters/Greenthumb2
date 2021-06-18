2bbb4617daab4f9dde4a0cac604904ab83dda9ed
Jason Anderson <jas.nanders.n@gmail.com>
2021-06-17 17:54:37 -0700

    started PlantCard

---
+++ src/components/PlantCard/PlantCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function PlantCard() {
+    return (
+        <div class="container is-fuild">
+            <div>
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PlantCard;
+
+// SLIDE 9
+
+// write out code to import plant information from plant database
