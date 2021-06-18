af88b008a682b871fb91787f15d113a6ca0a77b1
Jason Anderson <jas.nanders.n@gmail.com>
2021-06-17 17:58:14 -0700

    started UserCard

---
+++ src/components/UserCard/UserCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function UserCard() {
+    return (
+        <div class="container is-fuild">
+            <div>
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserCard;
+
+// SLIDE 11
+
+// write out code to import user information from user database
