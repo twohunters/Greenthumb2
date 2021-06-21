1b2a47e6aa3bd8a125df342a3e266ede4f1cfd4a
Antonio Barrera <Barrera.antonio@gmail.com>
2021-06-21 13:37:24 -0700

    garden view js

--- src/components/GardenView/index.js
+++ src/components/GardenView/index.js
@@ -1,1 +1,15 @@
 //slide 15
+import React from "react";
+import { Card } from "react-bulma-components";
+const {Image} = Card;
+
+function GardenView(props){
+    return(
+        <Card>
+            <Card.Image
+            size="4by3"
+        src={props.imageSrc}
+      />
+        </Card>
+    )
+}
