830b605ace83ca9895687b009a327bfd43df79ac
Jason Anderson <jas.nanders.n@gmail.com>
2021-06-21 20:02:31 -0700

    built UserFeed, additions to PlantForm

--- src/components/PlantForm/PlantForm.js
+++ src/components/PlantForm/PlantForm.js
@@ -8,43 +8,43 @@ function PlantForm() {
         <Form>
             <Form.Field>
                 <Form.Label>Plant Name</Form.Label>
-                <Form.Input type="text"></Form.Input>
+                <Form.Input type="text" name="plantName"></Form.Input>
             </Form.Field>
             <Form.Field>
                 <Form.Label>Description</Form.Label>
-                <Form.Input type="text"></Form.Input>
+                <Form.Input type="text" name="description"></Form.Input>
             </Form.Field>
             <Form.Field>
                 <Form.Label>Habit</Form.Label>
-                <Form.Input type="text"></Form.Input>
+                <Form.Input type="text" name="habit"></Form.Input>
             </Form.Field>
             <Form.Field>
                 <Form.Label>Life Cycle</Form.Label>
-                <Form.Input type="text"></Form.Input>
+                <Form.Input type="text" name="lifeCycle"></Form.Input>
             </Form.Field>
             <Form.Field>
                 <Form.Label>Sun Requirements</Form.Label>
-                <Form.Input type="text"></Form.Input>
+                <Form.Input type="text" name="sunReq"></Form.Input>
             </Form.Field>
             <Form.Field>
                 <Form.Label>Water Requirements</Form.Label>
-                <Form.Input type="text"></Form.Input>
+                <Form.Input type="text" name="waterReq"></Form.Input>
             </Form.Field>
             <Form.Field>
                 <Form.Label>Uses</Form.Label>
-                <Form.Input type="text"></Form.Input>
+                <Form.Input type="text" name="uses"></Form.Input>
             </Form.Field>
             <Form.Field>
                 <Form.Label>Edible</Form.Label>
-                <Form.Input type="text"></Form.Input>
+                <Form.Input type="text" name="edible"></Form.Input>
             </Form.Field>
             <Form.Field>
                 <Form.Label>Time to Fruit</Form.Label>
-                <Form.Input type="text"></Form.Input>
+                <Form.Input type="text" name="time"></Form.Input>
             </Form.Field>
             <Form.Field>
                 <Form.Label>Recipes</Form.Label>
-                <Form.Input type="text"></Form.Input>
+                <Form.Input type="text" name="recipes"></Form.Input>
             </Form.Field>
         </Form>
     )
--- src/components/UserFeed/UserFeed.js
+++ src/components/UserFeed/UserFeed.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Content } from "react-bulma-components";
+import { Heading } from "react-bulma-components";
+import { Box } from "react-bulma-components";
+// import user data from user database
 
 function UserFeed() {
     return (
-        <div>
-
-        </div>
+        <Content>
+            <Heading>
+                Your Account
+            </Heading>
+            <Heading subtitle>
+                {user.firstName} {user.lastName}
+            </Heading>
+            <Box>
+                <h3>About You</h3>
+                <p>{user.aboutYou}</p>
+            </Box>
+            <Box>
+                <h3>Your Gardens</h3>
+                <div>{user.gardens}</div>
+            </Box>
+        </Content>
     )
 }
 
