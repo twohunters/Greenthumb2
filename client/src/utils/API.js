25eb7dd55d8a8851b864247384e08865edf5de51
Antonio Barrera <Barrera.antonio@gmail.com>
2021-06-24 11:23:31 -0700

    src api's

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,18 +1,61 @@
 import axios from "axios";
 
 export default {
-    //save plant to DB
-    savePlant: function(plantData){
-        return axios.post("/api/plants", plantData);
+    //weather api
+    cityWeather: function(city){
+        return axios.get('https://api.openweathermap.org/data/2.5/weather?q='+city+ '&units=imperial&appid=cdc889a6c131e078f10ee5db1d9fa2e8')
     },
+
     //get all plants
-    getPlant: function(){
-        return axios.get("/api/plants/");
+    getPlant: function () {
+        return axios.get("/api/plants");
     },
     //get plant by id
-    getPlant: function(id){
+    getPlant: function (id) {
         return axios.get("/api/plants/" + id);
+    },
+    //delete plant by id
+    deletePlant: function(id){
+        return axios.delete("/api/plants" + id);
+    },
+    //save plant to DB
+    savePlant: function (plantData) {
+        return axios.post("/api/plants", plantData);
+    },
+
+
+    //get all gardens
+    getGarden: function (){
+        return axios.get("/api/gardens");
+    },
+    //get garden by id
+    getGarden: function(id){
+        return axios.get("/api/gardens/" + id);
+    },
+    //delete garden by id
+    deleteGarden: function(id){
+        return axios.delete("/api/gardens/" + id);
+    },
+    //save garden to db
+    saveGarden: function(gardenData){
+        return axios.post("/api/gardens", gardenData);
+    },
+
+
+    //get all users
+    getUser: function(){
+        return axios.get("/api/user");
+    },
+    //get user by ud
+    getUser: function(id){
+        return axios.get("api/user/" + id);
+    },
+    //delete user by id
+    deleteUser: function(id){
+        return axios.delete("api/user/" + id);
+    },
+    //save user to db
+    saveUser: function(userData){
+        return axios.post("/api/user", userData)
     }
-
-
-}
+};
