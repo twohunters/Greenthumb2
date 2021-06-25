3b9170feade641b58e4b311642f69395bbd7c081
Rayna W <raynagweinreb@gmail.com>
2021-06-24 18:23:59 -0700

    add seed file

---
+++ Seeder/seed.js
@@ -0,0 +1,14 @@
+
+const mongoose = require('mongoose');
+const db = require('../models');
+
+mongoose.connect('mongodb://localhost/plants', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+const plantSeed [
+    {
+        
+    }
+]
--- src/components/GardenView/index.js
+++ src/components/GardenView/index.js
@@ -13,3 +13,4 @@ function GardenView(props){
         </Card>
     )
 }
+export default GardenView
