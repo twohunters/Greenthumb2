8e5ab89e49a1310e13fd14dccee545f01b879099
Jason Anderson <jas.nanders.n@gmail.com>
2021-07-01 20:13:26 -0700

    progress in UserFeed

--- client/src/pages/UserFeed.js
+++ client/src/pages/UserFeed.js
@@ -1,31 +1,38 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Content } from "react-bulma-components";
 import { Box } from "react-bulma-components";
+import { useParams } from "react-router-dom";
 import API from "../utils/API";
 import queryString from "query-string";
 import "./UserFeed.css";
 
-const UserFeed = props => {
-    const values = queryString.parse(window.location.search)
-    console.log(values)
-    API.getUser()
+const UserFeed = () => {
+
+    const [user, findUser] = useState()
+    const { id } = useParams()
+    useEffect(() => {
+        API.getUser(id)
+            .then(res => { console.log(user); findUser(res.data)})
+            .catch(err => console.log(err));
+    })
+
     return (
         <div className="container">
             <Content>
                 <Box className="section">
                     <h1 className="title"><span class="material-icons">person</span> Your Account</h1>
-                    <h2 className="subtitle">{/* {props.firstName} {props.lastName} */}</h2>
+                    {/* <h2 className="subtitle">{user.FirstName} {user.LastName}</h2> */}
                 </Box>
                 <Box>
                     <h1 className="title">About You</h1>
                     <div className="userContent">
-                        {/* <p>{props.userDescription}</p> */}
+                        {/* <p>{user.About}</p> */}
                     </div>
                 </Box>
                 <Box>
                     <h1 className="title">Your Gardens</h1>
                     <div className="userContent">
-                        {/* <p>{props.userGardens}</p> */}
+                        {/* <p>{user.userGardens}</p> */}
                     </div>
                 </Box>
             </Content>
