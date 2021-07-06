dbf18250bdb39af65459d63d64fe7a0253390cbe
Antonio Barrera <Barrera.antonio@gmail.com>
2021-07-05 18:02:43 -0700

    working on post

--- Models/garden.js
+++ Models/garden.js
@@ -5,10 +5,13 @@ const gardenSchema = new Schema({
     title: { type: String, required: true },
     user_id: { type: String, required: true },
     size: {type: String, require: true},
-    plant_id:{
+    // plants: [{
+    //     type: String, require:true
+    // }]
+    plant_id:[{
         type:[PlantSchema],
         default: undefined
-    }
+    }]
 })
 const Garden = mongoose.model("Garden", gardenSchema)
 module.exports = Garden
