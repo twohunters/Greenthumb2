beaf631c4faab8b1bcbe83fc2b141b1daf7661c6
Jason Anderson <jas.nanders.n@gmail.com>
2021-07-05 23:38:00 -0700

    styled UserFeed

--- client/src/pages/UserFeed.js
+++ client/src/pages/UserFeed.js
@@ -1,47 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Card } from "react-bulma-components";
+import { Block } from "react-bulma-components";
 import API from "../utils/API";
 import "./UserFeed.css";
 
 const UserFeed = () => {
     const [user, setUser] = useState({
-                _id:'',
-        firstName:'',
-        lastName:'',
-        email:'',
-        password: '',
-        about: '',
+        _id: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        about: "",
         Garden_id: []
     })
     const { id } = useParams()
     useEffect(() => {
         console.log(id)
         API.getUser(id)
-        .then(res =>  setUser(res.data))
-        .catch(err => console.log(err));
-    },[])
+            .then(res => setUser(res.data))
+            .catch(err => console.log(err));
+    }, [])
     return (
-        <h1>test {user.firstName} </h1> 
-        // <div className="container">
-        //     <Content>
-        //         <Box className="section">
-        //             <h1 className="title"><span class="material-icons">person</span> Your Account</h1>
-        //             <h2 className="subtitle">{user.FirstName} {user.LastName}</h2>
-        //         </Box>
-        //         <Box>
-        //             <h1 className="title">About You</h1>
-        //             <div className="userContent">
-        //                 <p>{user.About}</p>
-        //             </div>
-        //         </Box>
-        //         <Box>
-        //             <h1 className="title">Your Gardens</h1>
-        //             <div className="userContent">
-        //                 <p>{user.userGardens}</p>
-        //             </div>
-        //         </Box>
-        //     </Content>
-        // </div>
+        <div className="container">
+            <Card>
+                <Block>
+                    <h1 className="card-header-title"><span class="material-icons">person</span> Your Account</h1>
+                </Block>
+                <Block>
+                    <h1 className="title">{user.firstName} {user.lastName}</h1>
+                </Block>
+                <Block>
+                <h1 className="subtitle">About You</h1>
+                    <div className="userContent">
+                        <p>{user.about}</p>
+                    </div>
+                </Block>
+                <Block>
+                <h1 className="subtitle">Your Gardens</h1>
+                    <div className="userContent">
+                        <p>{user.Garden_id}</p>
+                    </div>
+                </Block>
+            </Card>
+        </div>
     )
 }
 
