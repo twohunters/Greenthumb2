fd575a3322be35b944948f165823d2e9e373b880
Jason Anderson <jas.nanders.n@gmail.com>
2021-07-06 17:50:04 -0700

    formatted PlantCard

--- client/src/components/PlantCard/index.js
+++ client/src/components/PlantCard/index.js
@@ -1,47 +1,46 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
+import API from "../../utils/API";
 import { Card } from "react-bulma-components";
-import API from '../../utils/API'
 import { useHistory } from "react-router-dom"
-
 
 const { Header, Title, Content } = Card;
 
 const PlantCard = props => {
     const history = useHistory()
-    const [plants,setPlants]=useState([])
-    useEffect(()=>{
-        getPlants()
-    },[])
-    function getPlants(){
-        API.getPlants()
-    
-         .then(res => {
-             console.log(res); setPlants(res.data)
-            }
-            ).catch(err=> console.log(err))
+    const [plants, setPlants] = useState([])
 
+    useEffect(() => {
+        getPlants()
+    }, [])
+    function getPlants() {
+        API.getPlants()
+            .then(res => {
+                console.log(res); setPlants(res.data)
+            })
+            .catch(err => console.log(err))
     }
     return (
         <div>
-            {plants.length ?(
+            {plants.length ? (
                 <Content>
-                    {plants.map(plant=>(
+                    {plants.map(plant => (
                         <Card>
-                <Card.Header>
-                    <Card.Header.Title onClick={()=> history.push('/Singleplant/'+plant._id)} >
-                    <span class="material-icons">yard</span> {plant.name}
-                    </Card.Header.Title>
-                </Card.Header>
-                <Card.Content>
-                    {plant.description}
-                </Card.Content>
-            </Card>
-            ))}
-            </Content>
-            ):(<h3>No results to display</h3>)}
+                            <Card.Header>
+                                <Card.Header.Title onClick={() => history.push("Singleplant/" + plant._id)} >
+                                    <span class="material-icons">yard</span> {plant.name}
+                                </Card.Header.Title>
+                            </Card.Header>
+                            <Card.Content>
+                                {plant.description}
+                            </Card.Content>
+                        </Card>
+                    ))}
+                </Content>
+            ) : (<h3>No results to display</h3>)}
         </div>
-        )}
+    )
+}
 
 export default PlantCard;
 
